e7efe4b0ef0e4de2acff79d8411223c7ad22c7ba
Sensei-Says <mrpipiskin008@gmail.com>
2025-10-16 22:49:33 +0300

    Fix base path for GitHub Pages - remove /anime-watchlist/ prefix

---
+++ assets/GetBody-CWriphxC.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./index-msPDd8e2.js";import"./vendor-qYA6LsDu.js";export{m as default};
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory('/anime-watchlist/'),
+  history: createWebHistory('/'),
   routes: [
     {
       path: '/',
